1b25c5e86fad2f162b3194eefa7c0792e2fbc731
lvlivingston <lvlivingston@gmail.com>
2023-11-14 12:51:48 -0600

    completed lesson

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,13 +1,28 @@
 // IMPORT useEffect and useRef
-import { useState} from 'react';
+import { useState, useEffect } from 'react';
 import * as usersAPI from '../utilities/users-api'
 import './App.css';
 
 function App() {
 	const [userData, setUserData] = useState([]);
-  // ADD useRef
+	// ADD useRef
 
-  // ADD useEffect
+	// ADD useEffect
+	useEffect(function() {
+		async function getUsers() {
+		const users = await usersAPI.getUsers();
+		setUserData(users);
+		}
+		getUsers();
+	}, []);
+
+	// Unique countries from userData
+	const countriesList = userData.reduce((acc, user) => {
+		if (user.location && user.location.country && !acc.includes(user.location.country)) {
+			acc.push(user.location.country);
+		}
+		return acc;
+		}, []);
 
 	return (
 		<div>
@@ -18,7 +33,10 @@ function App() {
 					<section>
 						<h2 id='countries'>Countries</h2>
 						<ul aria-labelledby='countries'>
-              {/* RENDER THE LIST OF COUNTRY NAMES */}
+              				{/* RENDER THE LIST OF COUNTRY NAMES */}
+							  {countriesList.map((country, index) => (
+                					<li key={index}>{country}</li>
+              					))}
 						</ul>
 					</section>
 					<section>
